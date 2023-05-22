80bcb81782a432a3a4bc8e5f4f5fda7730a60b6c
Vinch <vince.daviaud@gmail.com>
2023-05-22 12:21:17 +0200

    exercice done

--- movieHandlers.js
+++ movieHandlers.js
@@ -166,6 +166,42 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("DELETE FROM movies WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("DELETE FROM users WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -175,4 +211,6 @@ module.exports = {
   postUser,
   updateMovie,
   updateUser,
+  deleteMovie,
+  deleteUser,
 };
